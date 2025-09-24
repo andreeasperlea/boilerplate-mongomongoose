4a93cb209c11c68d80dff8b49b2f64190313aec1
Andreea Sperlea <sperleaandreea2@gmail.com>
2025-09-24 20:48:42 +0300

    Created schema

--- myApp.js
+++ myApp.js
@@ -1,13 +1,19 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
-mongoose.connect(process.env.MONGO_URI, { 
-  useNewUrlParser: true, 
-  useUnifiedTopology: true 
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
 
+const personSchema=new Schema({
+  name:String,
+  age:Number,
+  favoriteFoods:[String]
+})
 
-let Person;
+let Person=mongoose.model('Person', personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
