82dee22f165dca77d2e4c192b5371f85b0c8ecb1
Andreea Sperlea <sperleaandreea2@gmail.com>
2025-09-25 20:19:45 +0300

    id2

--- myApp.js
+++ myApp.js
@@ -66,10 +66,10 @@ const findOneByFood = (food, done) => {
 };
 
 const findPersonById = (personId, done) => {
- Person.findById({_id:personId},(err,data)=>{
-    if(err) return done(err);
-    return done(null,data);
-  })
+  Person.findById(personId, (err, data) => {
+    if (err) return done(err);
+    return done(null, data);
+  });
 };
 
 const findEditThenSave = (personId, done) => {
