c869d8db75806796d26677af57f82dbbfeead356
Andreea Sperlea <sperleaandreea2@gmail.com>
2025-09-26 14:33:27 +0300

    remove

--- myApp.js
+++ myApp.js
@@ -99,7 +99,10 @@ const findAndUpdate = (personName, done) => {
 };
 
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findByIdAndRemove(personId,(err,data)=>{
+    if (err) return done(err);
+    return done(null, data);
+  })
 };
 
 const removeManyPeople = (done) => {
